b9c2186a5a1c00f46e74600f7a1ab6b2362721f0
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2022-01-30 22:23:44 +1100

    Update build.sh
    Added `-std=c++11` addressing issue #6

--- build.sh
+++ build.sh
@@ -35,10 +35,10 @@ case $1 in
     echo "Linux Build"
     # Linux
     echo "BUILDING THE MetaBCC-LR 15 MER COMPUTATIONS"
-    g++ mbcclr_utils/search-15mers.cpp -fopenmp -lpthread -Wall -o mbcclr_utils/bin/search-15mers -lz -O3
-    g++ mbcclr_utils/count-15mers.cpp -fopenmp -lpthread -Wall -o mbcclr_utils/bin/count-15mers -lz -O3
+    g++ mbcclr_utils/search-15mers.cpp -fopenmp -lpthread -Wall -o mbcclr_utils/bin/search-15mers -lz -O3 -std=c++11
+    g++ mbcclr_utils/count-15mers.cpp -fopenmp -lpthread -Wall -o mbcclr_utils/bin/count-15mers -lz -O3 -std=c++11
     echo "BUILDING THE MetaBCC-LR 3 MER COMPUTATIONS"
-    g++ mbcclr_utils/count-kmers.cpp -Wall -fopenmp -lpthread -o mbcclr_utils/bin/count-kmers -lz -O3
+    g++ mbcclr_utils/count-kmers.cpp -Wall -fopenmp -lpthread -o mbcclr_utils/bin/count-kmers -lz -O3 -std=c++11
     echo "BUILD FINISHED"
     ;;
 esac
